f048817d04f0131fa94aa8b73900eb192ea49a09
Vera Lyu <465784942@qq.com>
2023-02-28 16:37:12 +1100

    I have fun

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById("button");
+    input.addEventListener("click", function() {
+        alert("I was clicked!");
+    })
 }
+
+
+
+// function clickAlert() {
+//     alert("I was clicked too");
+// }
+
+// const tryOut = document.getElementById("main");
+// tryOut.addEventListener("click", clickAlert);
